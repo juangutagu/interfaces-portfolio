2549a072339093e9650a3881b0bca00b47d9d8ee
juangutierreza <juanguag@gmail.com>
2023-01-18 21:22:51 +0100

    replay

--- routes/proyectos/video/script.js
+++ routes/proyectos/video/script.js
@@ -1,30 +1,30 @@
-const myVideo = document.querySelector('#myVideo');
-const playPauseButton = document.querySelector('#playPause');
-const muteButton = document.querySelector('#mute');
-const fullScreenButton = document.querySelector('#fullScreen');
-const volumeBar = document.querySelector('#volumeBar');
-const progressBar = document.querySelector('#progressBar');
+const myVideo = document.querySelector("#myVideo");
+const playPauseButton = document.querySelector("#playPause");
+const muteButton = document.querySelector("#mute");
+const fullScreenButton = document.querySelector("#fullScreen");
+const volumeBar = document.querySelector("#volumeBar");
+const progressBar = document.querySelector("#progressBar");
 
 const initialConfig = {
-  volume: 0.20,
+  volume: 0.2,
   muted: false,
   paused: true,
-  fullScreen: false
-}
+  fullScreen: false,
+};
 
-playPauseButton.addEventListener('click', playPauseVideo);
-muteButton.addEventListener('click', muteVideo);
-fullScreenButton.addEventListener('click', fullScreenVideo);
-volumeBar.addEventListener('change', changeVolume);
-progressBar.addEventListener('change', changeProgressBar);
+playPauseButton.addEventListener("click", playPauseVideo);
+muteButton.addEventListener("click", muteVideo);
+fullScreenButton.addEventListener("click", fullScreenVideo);
+volumeBar.addEventListener("change", changeVolume);
+progressBar.addEventListener("change", changeProgressBar);
 
-myVideo.addEventListener('timeupdate', function() {
+myVideo.addEventListener("timeupdate", function () {
   const percentage = Math.floor((100 / myVideo.duration) * myVideo.currentTime);
-  document.querySelector('#progressBar').value = percentage;
-  document.querySelector('#progressBar').innerHTML = percentage + '% played';
+  document.querySelector("#progressBar").value = percentage;
+  document.querySelector("#progressBar").innerHTML = percentage + "% played";
+
+  if (myVideo.ended) playPauseButton.innerHTML = "Replay";
 });
-
-
 
 window.onload = loadInitialConfig();
 
@@ -35,29 +35,28 @@ function loadInitialConfig() {
   myVideo.fullScreen = initialConfig.fullScreen;
   volumeBar.value = initialConfig.volume;
 
-  playPauseButton.innerHTML = 'Play';
-  muteButton.innerHTML = 'Mute';
-  fullScreenButton.innerHTML = 'Full Screen';
+  playPauseButton.innerHTML = "Play";
+  muteButton.innerHTML = "Mute";
+  fullScreenButton.innerHTML = "Full Screen";
 }
-
 
 function playPauseVideo() {
   if (myVideo.paused) {
     myVideo.play();
-    playPauseButton.innerHTML = 'Pause';
+    playPauseButton.innerHTML = "Pause";
   } else {
     myVideo.pause();
-    playPauseButton.innerHTML = 'Play';
+    playPauseButton.innerHTML = "Play";
   }
 }
 
 function muteVideo() {
   if (myVideo.muted) {
     myVideo.muted = false;
-    muteButton.innerHTML = 'Mute';
+    muteButton.innerHTML = "Mute";
   } else {
     myVideo.muted = true;
-    muteButton.innerHTML = 'Unmute';
+    muteButton.innerHTML = "Unmute";
   }
 }
 
