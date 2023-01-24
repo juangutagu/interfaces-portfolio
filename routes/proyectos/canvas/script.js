7a76f91bc90589d280fd981851f437b6a1d5e7e2
juangutierreza <juanguag@gmail.com>
2023-01-24 16:37:58 +0100

    juan written on canvas

--- routes/proyectos/canvas/script.js
+++ routes/proyectos/canvas/script.js
@@ -21,3 +21,39 @@ ctx.beginPath();
 ctx.strokeStyle = "black";
 ctx.lineWidth = 3;
 ctx.strokeRect(110, 100, 50, 50);
+
+// Write your name
+const canvas2 = document.getElementById("myCanvas2");
+const ctx2 = canvas2.getContext("2d");
+
+ctx2.beginPath();
+ctx2.fillStyle = "#1d4e89";
+
+// J
+ctx2.moveTo(60, 40);
+ctx2.lineTo(60, 80);
+ctx2.lineTo(40, 80);
+ctx2.lineTo(40, 70);
+ctx2.moveTo(50, 40);
+
+// U
+ctx2.moveTo(70, 40);
+ctx2.lineTo(70, 80);
+ctx2.lineTo(90, 80);
+ctx2.lineTo(90, 40);
+
+// A
+ctx2.moveTo(100, 80);
+ctx2.lineTo(100, 40);
+ctx2.lineTo(120, 40);
+ctx2.lineTo(120, 80);
+ctx2.moveTo(100, 60);
+ctx2.lineTo(120, 60);
+
+// N
+ctx2.moveTo(130, 80);
+ctx2.lineTo(130, 40);
+ctx2.lineTo(150, 80);
+ctx2.lineTo(150, 40);
+
+ctx2.stroke();
