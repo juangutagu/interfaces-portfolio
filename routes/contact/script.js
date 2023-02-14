eacf8e89f462c45d3e8d17618fe993c8690121e4
juangutierreza <juanguag@gmail.com>
2023-02-14 18:50:03 +0100

    clean

--- routes/contact/script.js
+++ routes/contact/script.js
@@ -1,5 +1,4 @@
 function enviarMail() {
-  console.log("EMAIL");
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let message = document.getElementById("message").value;
